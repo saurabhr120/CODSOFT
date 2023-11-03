5b60352137e20ca9903bd7c653a42251b33fe531
saurabh advani <93053301+saurabhr120@users.noreply.github.com>
2023-11-03 16:01:39 +0530

    internship project

---
+++ Landing page/script.js
@@ -0,0 +1,11 @@
+{/* <script>
+        document.getElementById("showOverlayButton").addEventListener("click", function () {
+            const whiteOverlay = document.getElementById("whiteOverlay");
+            whiteOverlay.style.display = "block";
+
+            // To ensure the overlay is visible, use a small delay (adjust as needed)
+            setTimeout(function () {
+                whiteOverlay.style.opacity = 1;
+            }, 10);
+        });
+    </script> */}
